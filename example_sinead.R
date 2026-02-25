d2076d42df8fc23d3d3cdb5aef1758cdb40bafbd
Ryan Donovan <ryan.br.donovam@gmail.com>
2026-02-25 16:53:11 +0000

    changes activity 7

---
+++ example_sinead.R
@@ -0,0 +1,3 @@
+library(readr) #package to load in my dataset using the import function
+burnout <- read_csv("datasets/burnout.csv") #load in my dataset and call it burnout
+View(burnout) #check my dataset
